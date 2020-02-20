73d80167fe9243dd4eefb701a49c2f366c26639d
senyaq <senyaq@gmail.com>
2020-02-20 14:06:19 +0200

    added ArticleItem

---
+++ components/ArticleItem.js
@@ -0,0 +1,95 @@
+import React, { useState, useCallback, useContext, useRef } from 'react'
+import { StyleSheet, Image, Text, View, RefreshControl, Dimensions, SafeAreaView, ScrollView, TouchableWithoutFeedback } from 'react-native'
+
+import Swipeable from 'react-native-swipeable'
+
+import Loader from './Loader'
+
+const ArticlesItem = ({ article, onhandlerPress, read, onDelete }) => {
+  const [loadDelete, setLoadDelete] = useState(true)
+  const { height, width } = Dimensions.get('screen')
+  const swipeable = useRef(null)
+
+  const { url, title, abstract, multimedia } = article
+  const rightButtons = read && [
+    <TouchableWithoutFeedback
+      onPress={() => {
+        handleDelete(url)
+      }}
+    >
+      <View style={styles.cover_delete}>
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>Delete</Text>
+      </View>
+    </TouchableWithoutFeedback>,
+  ]
+
+  const handleDelete = url => {
+    setLoadDelete(url)
+    onDelete(url)
+    swipeable && swipeable.current.recenter()
+  }
+
+  return (
+    <View style={{ width }} key={url}>
+      <Swipeable ref={swipeable} rightButtons={rightButtons} onSwipeStart={() => setTimeout(() => swipeable && swipeable.current.recenter(), 1000)}>
+        <TouchableWithoutFeedback onPress={() => onhandlerPress({ url, title, abstract, multimedia: [null, null, { url: multimedia[2].url }] })}>
+          <View style={read ? { ...styles.row, ...styles.read } : styles.row}>
+            {loadDelete === url && <Loader style={{ position: 'absolute', left: '48%', top: '40%' }} />}
+            <View style={{ width: (width / 100) * 23 }}>
+              <Image
+                style={{ flex: 1 }}
+                source={{
+                  uri: multimedia[2].url,
+                }}
+              />
+            </View>
+            <View style={{ width: (width / 100) * 77, paddingLeft: 5, paddingRight: 10, paddingBottom: 5 }}>
+              <Text style={styles.title}>{title}</Text>
+              <Text style={styles.abstract}>{abstract}</Text>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Swipeable>
+      <View style={{ height: 5 }} />
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 15,
+    fontWeight: '600',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  cover_delete: {
+    backgroundColor: 'red',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    paddingLeft: '5%',
+  },
+  row: {
+    marginBottom: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 3,
+    paddingRight: 5,
+    backgroundColor: '#e9e9e936',
+    flexDirection: 'row',
+  },
+  read: {
+    borderWidth: 2,
+    borderColor: '#dde4ff',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+})
+
+export default ArticlesItem
--- components/ArticlesList.js
+++ components/ArticlesList.js
@@ -1,19 +1,17 @@
 import React, { useState, useCallback, useContext } from 'react'
-import { StyleSheet, Image, Text, View, RefreshControl, Dimensions, SafeAreaView, ScrollView, TouchableWithoutFeedback } from 'react-native'
-
-import Swipeable from 'react-native-swipeable'
+import { StyleSheet, Image, Text, View, RefreshControl, SafeAreaView, ScrollView } from 'react-native'
 
 import Loader from './Loader'
 
 import AppContext from '../context/AppContext'
+import ArticleItem from './ArticleItem'
 
-const ArticlesList = ({ articles, onhandlerPress, read, onDelete }) => {
+const ArticlesList = props => {
+  const { articles, read } = props
   const [refreshing, setRefreshing] = useState(false)
   const [show, setShow] = useState(true)
-  const [loadDelete, setLoadDelete] = useState(true)
   const valueContext = useContext(AppContext)
   const { onReguestArticles } = valueContext
-  const { height, width } = Dimensions.get('screen')
 
   setTimeout(() => setShow(false), 2000)
 
@@ -28,54 +26,10 @@ const ArticlesList = ({ articles, onhandlerPress, read, onDelete }) => {
 
   return articles.length > 0 ? (
     <SafeAreaView>
-      <ScrollView
-        // horizontal
-        // pagingEnabled
-        contentContainerStyle={styles.scrollView}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-      >
+      <ScrollView contentContainerStyle={styles.scrollView} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
         <View>
           {articles.map(item => {
-            const { url, title, abstract, multimedia } = item
-            const rightButtons = read && [
-              <TouchableWithoutFeedback
-                onPress={() => {
-                  setLoadDelete(url)
-                  onDelete(url)
-                }}
-              >
-                <Text>Delete</Text>
-              </TouchableWithoutFeedback>,
-            ]
-            return (
-              <>
-                <View style={{ width }}>
-                  <Swipeable rightButtons={rightButtons}>
-                    <TouchableWithoutFeedback
-                      key={url}
-                      onPress={() => onhandlerPress({ url, title, abstract, multimedia: [null, null, { url: multimedia[2].url }] })}
-                    >
-                      <View style={read ? { ...styles.row, ...styles.read } : styles.row}>
-                        {loadDelete === url && <Loader style={{ position: 'absolute', left: '48%', top: '40%' }} />}
-                        <View style={{ width: (width / 100) * 23 }}>
-                          <Image
-                            style={{ flex: 1 }}
-                            source={{
-                              uri: multimedia[2].url,
-                            }}
-                          />
-                        </View>
-                        <View style={{ width: (width / 100) * 77, paddingLeft: 5 }}>
-                          <Text style={styles.title}>{title}</Text>
-                          <Text style={styles.abstract}>{abstract}</Text>
-                        </View>
-                      </View>
-                    </TouchableWithoutFeedback>
-                  </Swipeable>
-                  <View style={{ height: 5 }} />
-                </View>
-              </>
-            )
+            return <ArticleItem article={item} {...props} />
           })}
         </View>
       </ScrollView>
@@ -95,11 +49,18 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 5,
   },
+  cover_delete: {
+    backgroundColor: 'red',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    paddingLeft: '5%',
+  },
   row: {
     marginBottom: 0,
     paddingTop: 0,
     paddingBottom: 0,
-    paddingLeft: 5,
+    paddingLeft: 3,
     paddingRight: 5,
     backgroundColor: '#e9e9e936',
     flexDirection: 'row',
