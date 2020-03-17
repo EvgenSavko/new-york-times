4a17c9d37fb8c745003fe9fa29d9a96abbf0b07e
sergey-sedykh <sedukhsergey@gmail.com>
2020-03-17 16:59:53 +0200

    update user photo and name in Profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,38 +1,89 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet } from 'react-native'
-import { Container, Text, Form, Item, Input, Label } from 'native-base'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { Container, Text, Form, Item, Input, Label, Button, Content } from 'native-base'
 import app from 'firebase/app'
 import { withRouter } from 'react-router-native'
 import { ImageUploader } from '../modules';
+import { usersDB } from '../lib/firebase'
 
 const UserProfile = ({
   history
 }) => {
-  const [state, setState] = useState({ currentUser: null })
-  const [userEmail, setUserEmail] = useState('')
+  const [user, setUser] = useState({
+    uid: null,
+    name: '',
+    image: '',
+  })
 
   useEffect(() => {
     const { currentUser } = app.auth()
     if (!currentUser) history.push('/login')
-    setState({ currentUser })
-    setUserEmail(currentUser.email)
+    const docUser = usersDB.doc(currentUser.uid)
+    docUser.get()
+      .then(user => {
+      setUser({
+        uid: currentUser.uid,
+        name: user.data().name,
+        image: user.data().image,
+      })
+    })
+      .catch(err => {
+        console.log('get user err',err)
+      })
   }, [])
+
+  const handleSaveUserData = () => {
+    if (user.uid) {
+      const docUser = usersDB.doc(user.uid);
+      docUser.update({
+        image: user.image,
+        name: user.name
+      })
+        .then(data => {
+          console.log('save success')
+        })
+        .catch(err => {
+          console.log('save error',err)
+        })
+    }
+  }
+
   return (
     <Container>
       <Form>
         <Item >
-          <Label>Email</Label>
-          <Input style={styles.input} value={userEmail} onChangeText={setUserEmail}/>
+          <Label>Name</Label>
+          <Input style={styles.input} value={user.name} onChangeText={name => setUser(state => ({
+            ...state,
+            name
+          }))}/>
         </Item>
       </Form>
-      <ImageUploader />
+      <ImageUploader
+        handleChangeImage={image => setUser(state => ({
+          ...state,
+          image
+        }))}
+        image={user.image}
+      />
+      {user.image ? (
+        <View style={styles.successContainer}>
+          <Button success onPress={handleSaveUserData}><Text>Save changes</Text></Button>
+        </View>
+      ) : null}
     </Container>
   )
 }
 
 const styles = StyleSheet.create({
   input: {
-    textAlign: 'center',
+    textAlign: 'left',
+  },
+  successContainer: {
+    marginTop: 40,
+    marginRight: 40,
+    marginLeft: 40,
+    marginBottom: 40,
   }
 })
 
